ee5ec92247ba79fb981b3040d27898869ac2d4a7
irang <irang0426@gmail.com>
2025-05-20 13:59:53 +0900

    update: UserDto use Lombok

--- src/main/java/com/github/irang/checkpoint/controller/AuthController.java
+++ src/main/java/com/github/irang/checkpoint/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.github.irang.checkpoint.controller;
 
+import com.github.irang.checkpoint.dto.UserDto;
 import com.github.irang.checkpoint.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/github/irang/checkpoint/dto/UserDto.java
+++ src/main/java/com/github/irang/checkpoint/dto/UserDto.java
@@ -1,49 +1,15 @@
 package com.github.irang.checkpoint.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class UserDto {
     private String username;
     private String email;
     private String password;
     private String nickname;
-
-    public UserDto() {}
-
-    public UserDto(String username, String email, String password, String nickname) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.nickname = nickname;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
 }
