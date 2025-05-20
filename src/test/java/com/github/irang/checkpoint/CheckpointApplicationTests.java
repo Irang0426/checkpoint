effc09c9f7e967cc20e29268e1cf8d4364908b63
irang <irang0426@gmail.com>
2025-05-20 09:34:39 +0900

    first commit

---
+++ src/test/java/com/github/irang/checkpoint/CheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.irang.checkpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
